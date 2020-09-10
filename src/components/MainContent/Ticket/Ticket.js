cb4918172a6974c35f7fd35c2a068a3c68a4fbfa
josip <josip.glasnovic04@gmai.com>
2020-09-10 15:56:03 +0200

    finishing ticket item

--- src/components/MainContent/Ticket/Ticket.js
+++ src/components/MainContent/Ticket/Ticket.js
@@ -5,17 +5,40 @@ const Ticket = () => {
     return (
         <div className="Ticket__container">
             <div className="Ticket__top">
-                <div className="id">
+                <div className="Ticket__id__container">
                     <p className="Ticket__top__id">#IT00006790</p>
                     <p className="Ticket__top__status">NEW</p>
                 </div>
-                <div className="text">
-                
-
+                <div className="Ticket__text__container">
+                    <p className="Ticket__top__subject">I cannot upload a non .PNG image onto my widget? <span className="Ticket__top__subject__time">3 minutes ago</span></p>
+                    <p className="Ticket__top__description">I cannot upload a non .PNG image onto my widget. Does your widget support .PNG?</p>
                 </div>
-
             </div>
             <div className="Ticket__bottom">
+                <div className="pinned">
+                    <i class="fas fa-thumbtack"></i>
+                </div>
+                <div className="favourite">
+                    <i class="fas fa-star"></i>
+                </div>
+                <div className="more">
+                    <i class="fas fa-ellipsis-h"></i>
+                </div>
+                <div className="assignedTo">
+                    <i class="fas fa-ellipsis-h"></i>
+                </div>
+                <div className="raisedBy">
+                    <i class="fas fa-ellipsis-h"></i>
+                </div>
+                <div className="priority">
+                    <i class="fas fa-ellipsis-h"></i>
+                </div>
+                <div className="category">
+                    <i class="fas fa-ellipsis-h"></i>
+                </div>
+                <div className="due">
+                    <i class="fas fa-ellipsis-h"></i>
+                </div>
 
             </div>
         </div>
@@ -24,5 +47,3 @@ const Ticket = () => {
  
 export default Ticket;
 
-
-//Zavrsi ticket item
